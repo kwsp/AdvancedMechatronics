37f7fb8ec2931ed244e23aa6ae2d664c16dc5eda
Tiger Nie <nhl0819@gmail.com>
2020-04-22 02:23:31 -0500

    hw2

--- HW2/HW2Code.X/main.c
+++ HW2/HW2Code.X/main.c
@@ -1,5 +1,8 @@
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
+#include <math.h>
+
+#include"spi.h" // SPI interface
 
 // DEVCFG0
 #pragma config DEBUG = OFF // disable debugging
@@ -23,14 +26,61 @@
 #pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
-#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz (8MHz/2 = 4MHz)
-#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV (4MHz * 24 = 96MHz)
-#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz (96MHz/2 = 48MHz)
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+
+#define CLOCK_RATE 48000000
 
 // DEVCFG3
 #pragma config USERID = 0 // some 16bit userid, doesn't matter what
 #pragma config PMDL1WAY = OFF // allow multiple reconfigurations
 #pragma config IOL1WAY = OFF // allow multiple reconfigurations
+
+void writeDAC_(unsigned char channel, unsigned int n) {
+    // Inputs: 
+    //      Channel: 0 for DACA, 1 for DACB
+    //      n range: 0-4096
+    //
+    // Control bits:
+    // bit 15 = 0 : Channel select
+    // bit 14 = 1 : Vref buffered
+    // bit 13 = 1 : 1x output gain
+    // bit 12 = 1 : Active mode operation, Vout available
+    unsigned char b1 = 0b01110000;
+
+    // Set the channel (A or B)
+    b1 |= (0b1 & channel) << 7;
+
+    // Get the upper 8 bits of the integer n
+    b1 |= (n >> 8) & 0b11111111;
+
+    // Cast the input int into an unsigned char
+    // to only keep the least significant 8 bits
+    unsigned char b2 = (unsigned char) n;
+
+    // Write 2 byte over SPI
+    // Bring CS low for two bytes as the 
+    // DAC's write command is 16 bits
+    LATAbits.LATA0 = 0; // Bring CS low
+    spi_io(b1);
+    spi_io(b2);
+    LATAbits.LATA0 = 1; // Bring CS high
+}
+
+
+void writeDAC_A(unsigned int n) {
+    writeDAC_(0, n);
+}
+
+void writeDAC_B(unsigned int n) {
+    writeDAC_(1, n);
+}
+
+void sleep_ms(double ms) {
+    _CP0_SET_COUNT(0);
+    while (_CP0_GET_COUNT() < CLOCK_RATE / 2000 * ms) {;}
+}
 
 int main() {
 
@@ -49,31 +99,45 @@ int main() {
     DDPCONbits.JTAGEN = 0;
 
     // do your TRIS and LAT commands here
+    
     TRISAbits.TRISA4 = 0; // A4 output: User LED
     TRISBbits.TRISB4 = 1; // B4 input: User button
+    
+    // Setup SPI1
+    initSPI();
 
     __builtin_enable_interrupts();
-
+    
+    unsigned int i = 0;
+    unsigned int flag = 0;
+    unsigned int sine_wave = 0;
+    unsigned int triangle_wave = 0;
+    
     while (1) {
         // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
         // remember the core timer runs at half the sysclk
-
-        if (PORTBbits.RB4 == 0) {
-            // When User (B4) button is pressed,
-            // Turn on A4 for 0.5s, off for 0.5s, on for 0.5s, off for 0.5s
-            _CP0_SET_COUNT(0); // CP0 counts at half the clock rate
-            LATAbits.LATA4 = 1;
-            while (_CP0_GET_COUNT() < 12000000) {} // 0.5s delay
-            _CP0_SET_COUNT(0);
-            LATAbits.LATA4 = 0;
-            while (_CP0_GET_COUNT() < 12000000) {} // 0.5s delay
-            _CP0_SET_COUNT(0); // CP0 counts at half the clock rate
-            LATAbits.LATA4 = 1;
-            while (_CP0_GET_COUNT() < 12000000) {} // 0.5s delay
-            _CP0_SET_COUNT(0);
-            LATAbits.LATA4 = 0;
-            while (_CP0_GET_COUNT() < 12000000) {} // 0.5s delay
+        
+        i++;
+        // Generate sine wave between 0 - 2048
+        sine_wave = 2048 * sin(6.2832 * 0.004 * i) + 2048;
+        writeDAC_A(sine_wave);
+        
+        sleep_ms(1); // 500 Hz for 2 2ms sleeps
+                
+        // Triangle wave
+        if (flag == 0) {
+            triangle_wave += 4;
+        } else {
+            triangle_wave -= 4;
         }
+        
+        if (triangle_wave >= 1000) flag = 1;
+        else if (triangle_wave == 0) flag = 0;
+        
+        writeDAC_B(4.096 * triangle_wave);
+
+        sleep_ms(1);
 
     }
 }
+
